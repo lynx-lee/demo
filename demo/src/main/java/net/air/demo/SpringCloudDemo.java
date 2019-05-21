cf9d7c8b62dac57cd0824aef2fb55f3a34bcd0f4
lynx-lee <lynx-lee@github.com>
2019-05-21 20:16:51 +0800

    提交Spring Cloud demo代码

---
+++ demo/src/main/java/net/air/demo/SpringCloudDemo.java
@@ -0,0 +1,13 @@
+package net.air.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemo.class, args);
+    }
+}
